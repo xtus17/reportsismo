bca32a3833d1329b8fef34bd1d88d749d067629e
xtus17 <cremagarrita@hotmail.com>
2024-12-07 01:26:22 -0500

    seven

--- sendNotifications.js
+++ sendNotifications.js
@@ -1,8 +1,3 @@
-const { Expo } = require("expo-server-sdk");
-const fetch = require("node-fetch");
-const expo = new Expo();
-
-const RTDatabase = "https://ajedrez-a65a1-default-rtdb.firebaseio.com/";
 
 const sendNotifications = async (customBody) => {
   console.log("Custom Body recibido:", customBody);
