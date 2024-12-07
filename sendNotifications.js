6eaeca87c8e76ba4709cbfacb7558ae2ce736fb9
xtus17 <cremagarrita@hotmail.com>
2024-12-07 11:56:21 -0500

    nine

--- sendNotifications.js
+++ sendNotifications.js
@@ -1,5 +1,5 @@
-const { Expo } = require("expo-server-sdk");
-const fetch = require("node-fetch");
+import { Expo } from "expo-server-sdk";
+import fetch from "node-fetch";
 
 const expo = new Expo();
 const RTDatabase = "https://ajedrez-a65a1-default-rtdb.firebaseio.com/";
