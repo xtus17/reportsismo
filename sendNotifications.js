705dd9bb0caee633b0606b661af21aa34da04295
xtus17 <cremagarrita@hotmail.com>
2024-12-09 22:44:02 -0500

    twenty six

--- sendNotifications.js
+++ sendNotifications.js
@@ -202,16 +202,16 @@ dotenv.config();
 
 const firebaseCredentials = {
   TYPE: process.env.TYPE,
-  PROJECT_ID:process.env.PROJECT_ID,
-  PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
-  PRIVATE_KEY: process.env.PRIVATE_KEY, 
-  CLIENT_EMAIL: process.env.CLIENT_EMAIL,
-  CLIENT_ID: process.env.CLIENT_ID,
-  AUTH_URI: process.env.AUTH_URI,
-  TOKEN_URI: process.env.TOKEN_URI,
-  AUTH_PROVIDER_X509_CERT_URL: process.env.AUTH_PROVIDER_X509_CERT_URL,
-  CLIENT_X509_CERT_URL: process.env.CLIENT_X509_CERT_URL,
-  UNIVERSE_DOMAIN: process.env.UNIVERSE_DOMAIN,
+  PROJECT_ID:process.env.PROJECTID,
+  PRIVATE_KEY_ID: process.env.PRIVATEKEYID,
+  PRIVATE_KEY: process.env.PRIVATEKEY, 
+  CLIENT_EMAIL: process.env.CLIENTEMAIL,
+  CLIENT_ID: process.env.CLIENTID,
+  AUTH_URI: process.env.AUTHURI,
+  TOKEN_URI: process.env.TOKENURI,
+  AUTH_PROVIDER_X509_CERT_URL: process.env.AUTHPROVIDERX509CERTURL,
+  CLIENT_X509_CERT_URL: process.env.CLIENTX509CERTURL,
+  UNIVERSE_DOMAIN: process.env.UNIVERSEDOMAIN,
 };
 
 import { Expo } from "expo-server-sdk";
